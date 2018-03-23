b01d669204a2c0d5ee3fdf3812b4f8690f6e2665
gabyslaw <gabyslaw@gmail.com>
2018-03-23 18:11:12 +0100

    Corrected the Tester Class

---
+++ Migrations/201803231701571_Initial1.cs
@@ -0,0 +1,31 @@
+namespace BlogApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Post_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Posts", t => t.Post_Id)
+                .Index(t => t.Post_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tags", "Post_Id", "dbo.Posts");
+            DropIndex("dbo.Tags", new[] { "Post_Id" });
+            DropTable("dbo.Tags");
+        }
+    }
+}
--- Tester.cs
+++ Tester.cs
@@ -14,13 +14,9 @@ namespace BlogApp
         {
             GetAll();
             GetSingle();
-
-            var context2 = new BlogRepository<BlogAppDbContext, Post>();
-            var query2 = context2.Add();
-            foreach (var item in query)
-            {
-                Console.WriteLine($"{item.Title} | {item.Body} | {item.CreatedDate}");
-            }
+            AddEdit();
+            Delete();
+            FindBy();
 
             Console.ReadLine();
         }
@@ -42,8 +38,42 @@ namespace BlogApp
 
         public static void AddEdit()
         {
-            var AddPost = new Post();
-            AddPost.Title = 
+            var Post1 = new Post()
+            {
+                Title = "This is a new post for the Add",
+                CreatedDate = DateTime.Now,
+                Body = "Lorem ipsum text is what i typed here"
+            };
+
+            var Post2 = new Post()
+            {
+                Title = "This is the second new post for the Edit",
+                CreatedDate = DateTime.Now,
+                Body = "Lorem ipsum text is what i typed here"
+            };
+
+            var context3 = new BlogRepository<BlogAppDbContext, Post>();
+            context3.Add(Post1);
+            Post1.Title = "This is the edited title";
+            context3.Edit(Post1);
+        }
+        private static void Delete()
+        {
+            var context = new BlogRepository<BlogAppDbContext, Post>();
+
+            context.Delete(1);
+
+        }
+
+        private static void FindBy()
+        {
+            var context = new BlogRepository<BlogAppDbContext, Post>();
+            var output = context.FindBy(c => c.Body.Contains("ipsum"));
+            foreach (var item in output)
+            {
+                Console.WriteLine($"{item.Id} | {item.Title} | {item.Body}");
+            }
+
         }
     }
 }
